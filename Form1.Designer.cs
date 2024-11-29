ff4855cb25aaf1647f224e686a419e0a7fa06a13
Joao Pedro <azuleishon.jp@hotmail.com>
2024-11-29 13:19:28 -0300

    Primeiro commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SemeaJoao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_perfil = new System.Windows.Forms.Button();
+            this.btn_beneficios = new System.Windows.Forms.Button();
+            this.btn_aprendizagem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_perfil
+            // 
+            this.btn_perfil.Location = new System.Drawing.Point(82, 255);
+            this.btn_perfil.Name = "btn_perfil";
+            this.btn_perfil.Size = new System.Drawing.Size(183, 127);
+            this.btn_perfil.TabIndex = 0;
+            this.btn_perfil.Text = "Exibir Perfil";
+            this.btn_perfil.UseVisualStyleBackColor = true;
+            // 
+            // btn_beneficios
+            // 
+            this.btn_beneficios.Location = new System.Drawing.Point(408, 255);
+            this.btn_beneficios.Name = "btn_beneficios";
+            this.btn_beneficios.Size = new System.Drawing.Size(183, 127);
+            this.btn_beneficios.TabIndex = 1;
+            this.btn_beneficios.Text = "Gerenciar Benefícios";
+            this.btn_beneficios.UseVisualStyleBackColor = true;
+            this.btn_beneficios.Click += new System.EventHandler(this.btn_beneficios_Click);
+            // 
+            // btn_aprendizagem
+            // 
+            this.btn_aprendizagem.Location = new System.Drawing.Point(734, 255);
+            this.btn_aprendizagem.Name = "btn_aprendizagem";
+            this.btn_aprendizagem.Size = new System.Drawing.Size(183, 127);
+            this.btn_aprendizagem.TabIndex = 2;
+            this.btn_aprendizagem.Text = "Aprendizagem";
+            this.btn_aprendizagem.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 636);
+            this.Controls.Add(this.btn_aprendizagem);
+            this.Controls.Add(this.btn_beneficios);
+            this.Controls.Add(this.btn_perfil);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_perfil;
+        private System.Windows.Forms.Button btn_beneficios;
+        private System.Windows.Forms.Button btn_aprendizagem;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace SemeaJoao
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_seusbeneficios = new System.Windows.Forms.Label();
+            this.txt_saude = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_seusbeneficios
+            // 
+            this.txt_seusbeneficios.AutoSize = true;
+            this.txt_seusbeneficios.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_seusbeneficios.Location = new System.Drawing.Point(12, 9);
+            this.txt_seusbeneficios.Name = "txt_seusbeneficios";
+            this.txt_seusbeneficios.Size = new System.Drawing.Size(185, 29);
+            this.txt_seusbeneficios.TabIndex = 0;
+            this.txt_seusbeneficios.Text = "Seus benefícios";
+            // 
+            // txt_saude
+            // 
+            this.txt_saude.AutoSize = true;
+            this.txt_saude.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_saude.Location = new System.Drawing.Point(13, 87);
+            this.txt_saude.Name = "txt_saude";
+            this.txt_saude.Size = new System.Drawing.Size(213, 20);
+            this.txt_saude.TabIndex = 1;
+            this.txt_saude.Text = "Plano de Saúde: HB Saúde";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Situação:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label2.Location = new System.Drawing.Point(14, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Exibir dependentes";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label3.Location = new System.Drawing.Point(14, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Exibir dependentes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Situação:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(252, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Plano Odontológico: OdontoPrev";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label6.Location = new System.Drawing.Point(14, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Exibir dependentes";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 310);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Situação:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(13, 287);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(276, 40);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Plano Farmacêutico: FuncionalCard\r\n\r\n";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label9.Location = new System.Drawing.Point(14, 426);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(123, 16);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Exibir dependentes";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(14, 410);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(63, 16);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Situação:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(13, 387);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(189, 20);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Seguro de Vida: Maphre\r\n";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 501);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_saude);
+            this.Controls.Add(this.txt_seusbeneficios);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txt_seusbeneficios;
+        private System.Windows.Forms.Label txt_saude;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
